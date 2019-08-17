f1e670834334581cdb8bd01aa052af37b907a33d
Francesca P <francescapuche@gmail.com>
2019-08-17 01:03:23 -0400

    added purchase questions

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -23,9 +23,9 @@ function start () {
 
     var query = "Select * FROM products";
 	connection.query(query, function(err, res){
-		console.log('------------------------------------------------------------------------');
-		console.log('           Please Choose From One Of The Following Categories           ');
-		console.log('------------------------------------------------------------------------');
+		console.log("------------------------------------------------------------------------");
+		console.log("           Please Choose From One Of The Following Categories           ");
+		console.log("------------------------------------------------------------------------");
 		if(err) throw err;
 		var displayTable = new Table ({
 			head: ["Item ID", "Product Name", "Department", "Price", "Inventory"],
@@ -36,6 +36,25 @@ function start () {
 				[res[i].item_id,res[i].product_name, res[i].department_name, res[i].price, res[i].stock_quantity]
 				);
 		}
-		console.log(displayTable.toString());
+        console.log(displayTable.toString());
+        console.log("------------------------------------------------------------------------");
+        purchaseQuestions();
+        
 	});
 }; 
+
+function purchaseQuestions(){
+	inquirer.prompt([
+	{
+		name: "ID",
+		type: "input",
+		message:"Please enter Item ID you would like to purhcase.",
+		filter:Number
+	},
+	{
+		name:"QTY",
+		type:"input",
+		message:"How many items do you wish to purchase?",
+		filter:Number
+    }
+}
